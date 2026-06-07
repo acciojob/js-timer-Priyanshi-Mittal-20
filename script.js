1595f9ce0a3a3b50ecacce66366572c9c9a5de22
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-06-07 14:56:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // write js code here if required
+let date =new Date()
+let day=date.getDay()
+let month=date.getMonth()
+let year=date.getYear()
+let hour=date.getHours()
+let minute=date.getMinutes()
+let second=date.getSeconds()
+let para=document.getElementById("timer")
+para.innerText=`${day}/${month}/${year}, ${hour}:${minute}:${second}`
